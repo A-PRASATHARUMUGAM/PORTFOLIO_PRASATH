f852eeb62f71f5cd584d139b193c3cdb3a2d031a
A-PRASATHARUMUGAM <prasatharumugam040@gmail.com>
2024-10-17 01:44:10 +0530

    compress image and responsive all Navbar

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+const headerLinks=document.querySelector(".header-links");
+const hamurgerMenu=document.querySelector(".hamburger-menu");
+hamurgerMenu.addEventListener("click",()=>{
+
+headerLinks.classList.toggle("showmenu")
+hamurgerMenu.classList.toggle("Ahmenu")
+
+})
